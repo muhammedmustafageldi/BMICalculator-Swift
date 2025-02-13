17b559e6f5c4b15109310cd6f392a33136ebeeee
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-02-13 15:10:34 +0300

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Muhammed Mustafa Geldi on 13.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BMICalculatorTests/BMICalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculatorTests.swift
+//  BMICalculatorTests
+//
+//  Created by Muhammed Mustafa Geldi on 13.02.2025.
+//
+
+import Testing
+@testable import BMICalculator
+
+struct BMICalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
